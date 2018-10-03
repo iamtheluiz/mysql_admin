0bdbd73ff522b768f48af0ab7b0e97bd1c609769
Luiz Gustavo da Silva Vasconcellos <iamtheluiz.dev@gmail.com>
2018-10-03 09:26:23 -0300

    Adicionados recursos iniciais

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<link href="css/material_icons.css" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+		<meta charset="UTF-8">
+		<title>Login | Mysql Admin</title>
+	</head>
+	<body>
+		<div class="content row">
+
+
+		</div>
+		<script type="text/javascript" src="js/jquery-1.12.0.min.js"></script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+	</body>
+</html>
